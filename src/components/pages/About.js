7725abce0c92a4c384b944b9a86383a63d404f52
scottjones <sj6018@ic.ac.uk>
2020-05-26 20:43:58 +0100

    should work

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <React.Fragment>
+    <h1>About</h1>
+    <h2>Note: this may appear quite slow as it is using server requests</h2>
+    <p>This is the TodoList app v1.0.0. To help me learn React.</p>
+    </React.Fragment>
+  );
+}
+
+export default About;
